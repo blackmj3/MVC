4baa5c259dae28f0c2b99a9e6ec9d152b6ea15ed
blackmj3 <blackmj123@gmail.com>
2025-06-03 22:32:09 +0300

    Befor update commit

--- ToDoSolution_SV/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
+++ ToDoSolution_SV/Web/obj/Debug/net9.0/Web.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Web")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f212ff7d91ea956390de799bad77d7547eb5eb67")]
 [assembly: System.Reflection.AssemblyProductAttribute("Web")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Web")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
